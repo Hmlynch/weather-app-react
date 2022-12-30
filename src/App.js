5a633661ce1916b612ba3f65bed74c6ad4a65d1f
Hayden Lynch <haydenlynch9803.prof@gmail.com>
2022-12-30 11:51:21 -0500

    weather application just in the app.js file

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios'
+
+function App() {
+
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState('')
+  
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=50c9f2b64591b303b60ec53b1d13e1dd`
+  
+
+  const searchLocation = (event) => {
+    if (event.key === 'Enter') {
+      axios.get(url).then((response) => {
+        setData(response.data)
+        console.log(response.data)
+      })
+      setLocation('')
+    }
+  }
+  
+  return (
+    <div className="app">
+      <div className="container">
+        <h1>Weather Application</h1>
+        <div className="top">
+          <div className="search">
+            <input type="text" value={location} onChange={event => setLocation(event.target.value)} onKeyPress={searchLocation} placeholder="Location" />
+          </div>
+        </div>
+
+        {data.name != undefined &&
+          <div className="bottom">
+            <div className="location">
+              <h1>{data.name}</h1>
+            </div>
+            <div className="high">
+              <h2>High</h2>
+              {data.main ? <p>{data.main.temp_max.toFixed()}°F</p> : null}
+            </div>
+            <div className="low">
+              <h2>Low</h2>
+              {data.main ? <p>{data.main.temp_min.toFixed()}°F</p> : null}
+            </div>
+            <div className="forecast">
+              <h2>Forecast</h2>
+              {data.weather ? <p>{data.weather[0].main}</p> : null}
+            </div>
+            <div className="humidity">
+              <h2>Humidity</h2>
+              {data.main ? <p>{data.main.humidity}</p> : null}
+            </div>
+          </div>
+        } 
+      </div>
+    </div>
+  );
+}
+
+export default App;
